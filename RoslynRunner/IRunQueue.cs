22463ff0d984edfcb6f9c267155a64c479be6f89
Al Lamarre <Al.Lamarre@D2L.com>
2025-02-10 10:47:49 -0500

    make rerunnable

--- RoslynRunner/IRunQueue.cs
+++ RoslynRunner/IRunQueue.cs
@@ -10,17 +10,20 @@ public interface IRunQueue
 {
     Task Enqueue(RunCommand runCommand, CancellationToken cancellationToken = default);
     Task<RunParameters> Dequeue(CancellationToken cancellationToken = default);
+    Task<bool> ReRunLastEnqueuedCommand(CancellationToken cancellationToken = default);
 }
 
 public class RunQueue : IRunQueue
 {
     private readonly Channel<RunParameters> _channel = Channel.CreateUnbounded<RunParameters>();
+    private RunCommand? lastEnqueuedCommand = null;
 
     public async Task Enqueue(RunCommand runCommand, CancellationToken cancellationToken = default)
     {
         using var activity = Activity.Current;
         var parameters = new RunParameters(runCommand, activity!.TraceId, activity!.SpanId);
         await _channel.Writer.WriteAsync(parameters, cancellationToken);
+        lastEnqueuedCommand = runCommand;
     }
 
     public async Task<RunParameters> Dequeue(CancellationToken cancellationToken = default)
@@ -28,5 +31,15 @@ public class RunQueue : IRunQueue
         var parameters = await _channel.Reader.ReadAsync(cancellationToken);
 
         return parameters;
+    }
+
+    public async Task<bool> ReRunLastEnqueuedCommand(CancellationToken cancellationToken)
+    {
+       if(lastEnqueuedCommand == null)
+        {
+            return false;
+        }
+        await Enqueue(lastEnqueuedCommand);
+        return true;
     }
 }
