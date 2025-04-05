af757cb897c6234e196dca3a1bdee42d1e69ddb8
Al Lamarre <alamarre@gmail.com>
2025-04-05 17:53:22 -0400

    run API tests on their own by default

---
+++ RoslynRunner.ApiTests/SampleRefactorTests.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Net.Http.Json;
+
+namespace RoslynRunner.ApiTests;
+
+public class SampleRefactorTests
+{
+
+    [Test]
+    public async Task ApiCanRunSampleConversion()
+    {
+        var baseDirectory = AppContext.BaseDirectory!;
+        var sampleRoot = Path.Combine(baseDirectory, "samples", "LegacyWebApp");
+        string targetFile = Path.Combine(sampleRoot, "ModernWebApi", "Endpoints", "SampleEndpoint.cs");
+        if (File.Exists(targetFile))
+        {
+            File.Delete(targetFile);
+        }
+        Assert.That(File.Exists(targetFile), Is.False);
+        var legacyWebappSln = Path.Combine(sampleRoot, "LegacyWebApp.sln");
+        Assert.That(File.Exists(legacyWebappSln), Is.True);
+
+        var legacyWebAppConverterCsproj = Path.Combine(sampleRoot, "LegacyWebAppConverter", "LegacyWebAppConverter.csproj");
+
+        RunCommand runCommand = new(
+            ProcessorSolution: legacyWebAppConverterCsproj,
+            ProcessorName: "LegacyWebAppConverter.ConvertToMinimalApi",
+            ProcessorProjectName: "LegacyWebAppConverter",
+            PrimarySolution: legacyWebappSln,
+            PersistSolution: false
+        );
+
+        var response = await AppContext.HttpClient.PostAsJsonAsync("http://localhost:5000/runs", runCommand);
+        Assert.That((int)response.StatusCode, Is.EqualTo(200));
+        var run = await response.Content.ReadFromJsonAsync<Run>();
+        Assert.That(run?.RunId, Is.Not.EqualTo(Guid.Empty));
+        var runResponse = await AppContext.HttpClient.GetAsync($"http://localhost:5000/runs/{run.RunId}");
+
+        Assert.That((int)runResponse.StatusCode, Is.EqualTo(200));
+
+        Assert.That(File.Exists(targetFile), Is.True);
+    }
+}
